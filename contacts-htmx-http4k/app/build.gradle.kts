30766bca523d71c6bdaccd85ead91724ed929994
akiraly <kiralyattila.hu@gmail.com>
2024-02-23 09:25:40 +0100

    Start using BOM-s for a consistent classpath

--- contacts-htmx-http4k/app/build.gradle.kts
+++ contacts-htmx-http4k/app/build.gradle.kts
@@ -6,10 +6,12 @@ plugins {
 }
 
 dependencies {
+  implementation(platform(libs.kotlin.bom))
   implementation(libs.bundles.hoplite)
   implementation(libs.kotlinx.html)
   implementation(libs.logback)
 
+  testImplementation(platform(libs.kotest.bom))
   testImplementation(libs.bundles.kotest)
 }
 
